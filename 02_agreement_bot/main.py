fac0ae086ab6277007b2938c424a1cf9bf2f7565
m_hammad-ur-rehman <164639583+hammadurrehman2006@users.noreply.github.com>
2025-04-19 08:53:00 +0500

    Create main.py

---
+++ 02_agreement_bot/main.py
@@ -0,0 +1,11 @@
+def main():
+    # Ask the user for their favorite animal
+    user_input = input("What's your favorite animal? ")
+    
+    # Respond with the message including the user's favorite animal
+    print(f"My favorite animal is also {user_input}!")
+
+# Run the program
+
+if __name__ == '__main__':
+    main()
